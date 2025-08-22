c795ff617cbf9ad24ad0d767580eb4fd7bfea275
oduagbondestiny <oduagondestiny@gmail.com>
2025-08-22 01:37:33 +0100

    pushing final build static files 6

--- shareit/settings.py
+++ shareit/settings.py
@@ -136,8 +136,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / "shareit" / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
